47d55888818e2c06dbc6402b78af6f0695ca7523
Verena Kaynig-Fittkau <vkaynig@seas.harvard.edu>
2015-01-28 10:52:49 -0500

    Added dropbox and piazza links

--- sitestuff.py
+++ sitestuff.py
@@ -7,10 +7,13 @@ CLASS_NUMBER="AM207"
 
 
 # Blogroll
-LINKS =  (('Piazza', 'https://piazza.com/harvard/spring2014/am207/home'),
-          ('Videos', 'http://cm.dce.harvard.edu/2015/'),
-          ('ISite', 'http://isites.harvard.edu/icb/icb.do?keyword=k102129&login=yes'),)
+LINKS =  (('Piazza', 'https://piazza.com/harvard/spring2015/am207/home'),
+          ('Live Classroom', 'http://cm.dce.harvard.edu/2015/02/24104/liveClassroom.shtml'),
+          ('Videos', 'http://cm.dce.harvard.edu/2015/02/24104/publicationListing.shtml'),
+          ('Dropbox', 'http://isites.harvard.edu/icb/icb.do?keyword=k109999&login=yes'),)
+          ('Dropbox DCE', 'http://isites.harvard.edu/icb/icb.do?keyword=k110304&pageid=icb.page727592&pageContentId=icb.pagecontent1592193&state=maximize'),)
 
+          
 OTHERLINKS = (('Numpy', 'http://docs.scipy.org/doc/numpy/reference/'),
           ('Scipy', 'http://docs.scipy.org/doc/scipy/reference/'),
           ('Pandas', 'http://pandas.pydata.org/pandas-docs/dev/'),
