443ad77d072a3c889f7f94f0551d2dee59f424b6
Verena Kaynig-Fittkau <vkaynig@seas.harvard.edu>
2015-01-28 16:38:32 -0500

    Added github link

--- sitestuff.py
+++ sitestuff.py
@@ -11,8 +11,8 @@ LINKS =  (('Piazza', 'https://piazza.com/harvard/spring2015/am207/home'),
           ('Live Classroom', 'http://cm.dce.harvard.edu/2015/02/24104/liveClassroom.shtml'),
           ('Videos', 'http://cm.dce.harvard.edu/2015/02/24104/publicationListing.shtml'),
           ('Dropbox', 'http://isites.harvard.edu/icb/icb.do?keyword=k109999&login=yes'),
-          ('Dropbox DCE', 'http://isites.harvard.edu/icb/icb.do?keyword=k110304&pageid=icb.page727592&pageContentId=icb.pagecontent1592193&state=maximize'),)
-
+          ('Dropbox DCE', 'http://isites.harvard.edu/icb/icb.do?keyword=k110304&pageid=icb.page727592&pageContentId=icb.pagecontent1592193&state=maximize'),
+          ('Github', 'https://github.com/AM207/2015'),)
           
 OTHERLINKS = (('Numpy', 'http://docs.scipy.org/doc/numpy/reference/'),
           ('Scipy', 'http://docs.scipy.org/doc/scipy/reference/'),
